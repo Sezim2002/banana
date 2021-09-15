b6a476ac91fea95983e92f0d9115d359949599a1
sezim <maitukinovva02@gmail.com>
2021-09-15 10:27:47 +0600

    Новый файл

---
+++ account/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+
+class Registration Serializer(serializers.Serializer):
+    pass
+
+
+class LoginSerializer(serializers.Serializer):
+    pass
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    pass
